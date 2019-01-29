caffecb3a371c2da5b60e04d34a181e84209a5ff
Jamie Brooks <jamiebrooks@Jamies-MacBook-Pro.local>
2019-01-28 19:14:09 -0500

    Reformated Libarary

--- Divisibility/src/com/company/Main.java
+++ Divisibility/src/com/company/Main.java
@@ -3,7 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
-	// write your code here
+
+       int divisibleNum = 2;
+
+
+       while()
+        for(int num1 = 2; num1 <= 20; num1++){
+            if(divisibleNum % num1 != 0){
+               // System.out.println(divisibleNum);
+                divisibleNum++;
+
+            }
+        }
+        System.out.println(divisibleNum);
+
     }
 }
